f90ace3f163daca8d03e2a45091c1c077c84bf58
francescjhernandez <francesc.j.hernandez@uv.es>
2026-07-26 17:57:51 +0200

    Add Portuguese language translations for subjects

--- locales/pt.js
+++ locales/pt.js
@@ -1,13 +1,15 @@
 export const pt = {
   form: {
     select_subject_placeholder: "Selecione uma disciplina...",
-    label_subject: "Disciplina"
+    label_subject: "Disciplina",
+    btn_upload_prompts: "Carregar Prompts"
   },
   subjects: {
     history: "História",
     geography: "Geografia",
     spanish_language: "Língua Espanhola e Literatura",
     valencian_language: "Língua Valenciana/Catalã e Literatura",
+    portuguese_language: "Língua Portuguesa e Literatura",
     english: "Língua Inglesa",
     french: "Língua Francesa",
     mathematics: "Matemática",
