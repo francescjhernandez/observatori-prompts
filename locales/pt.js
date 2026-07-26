958bf2ac5ac352070a7396700d643e900bd9a4db
francescjhernandez <francesc.j.hernandez@uv.es>
2026-07-26 17:41:16 +0200

    Update pt.js

--- locales/pt.js
+++ locales/pt.js
@@ -1,1 +1,24 @@
-
+export const pt = {
+  form: {
+    select_subject_placeholder: "Selecione uma disciplina...",
+    label_subject: "Disciplina"
+  },
+  subjects: {
+    history: "História",
+    geography: "Geografia",
+    spanish_language: "Língua Espanhola e Literatura",
+    valencian_language: "Língua Valenciana/Catalã e Literatura",
+    english: "Língua Inglesa",
+    french: "Língua Francesa",
+    mathematics: "Matemática",
+    biology_geology: "Biologia e Geologia",
+    physics_chemistry: "Física e Química",
+    philosophy: "Filosofia",
+    art_history: "História da Arte",
+    music: "Música",
+    technology: "Tecnologia e Engenharia",
+    physical_education: "Educação Física",
+    latin: "Latim",
+    greek: "Grego"
+  }
+};
